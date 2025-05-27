faee47aaf867feccd379d5b490561ed1a0dae519
Hexros Raymond <ngotanvn2001@gmail.com>
2025-05-28 00:41:04 +0700

    init

---
+++ userscript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Load Images in STV
+// @namespace   Load Images in STV
+// @match       (https://|http://)(14.225.254.182|sangtacviet.app|sangtacviet.vip)/truyen/*/1/*/*/
+// @exclude-match (https://|http://)(14.225.254.182|sangtacviet.app|sangtacviet.vip)/truyen/*/1/*/0/
+// @grant       none
+// @version     1.0
+// @author      Hexros Raymond
+// @icon        https://raw.githubusercontent.com/hexros-dev/load-imgs/master/assets/icons/icon_64.png
+// @downloadURL https://raw.githubusercontent.com/hexros-dev/load-imgs/master/userscript.js
+// @homepageURL https://github.com/hexros-dev/load-imgs
+// @supportURL  https://github.com/hexros-dev/load-imgs/issues
+// @description Load image tags when names are added to STV.
+// ==/UserScript==
